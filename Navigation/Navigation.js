b04941c44f044169680b96197219a7900a4dbd65
HarelElihu <58102410+HarelEl@users.noreply.github.com>
2021-12-05 15:05:13 +0200

    update
    Update Navigation
    Make HomeScreen for user after login

--- Navigation/Navigation.js
+++ Navigation/Navigation.js
@@ -1,7 +1,10 @@
 import { createStackNavigator } from 'react-navigation-stack'
 import { createAppContainer } from 'react-navigation';
 import Colors from '../constants/Colors'
-
+import signUp2 from '../screens/User/signUp2'
+import signUp1 from '../screens/Social/signUp1';
+import signUp3 from '../screens/Trans/signUp3';
+import signUp4 from '../screens/Club/signUp4';
 import Main from '../screens/Main'
 import LoginV2 from '../screens/LoginV2'
 
@@ -10,7 +13,10 @@ import LoginV2 from '../screens/LoginV2'
 const AppNavigator = createStackNavigator({
     Main:{ screen: Main ,headerTitle: 'EAR ME'},
     LoginV2:{ screen: LoginV2 ,headerTitle: 'EAR ME'},
-    
+    signUp1:{ screen: signUp1 ,headerTitle: 'EAR ME'},
+    signUp2:{ screen: signUp2 ,headerTitle: 'EAR ME'},
+    signUp3:{ screen: signUp3 ,headerTitle: 'EAR ME'},
+    signUp4:{ screen: signUp4 ,headerTitle: 'EAR ME'},
 },
 {
     defaultNavigationOptions : {
